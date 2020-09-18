2b08488521e53ad80715a9fb5f2c2b8d45ca0646
David Lara Diaz <david.lara01uptc.edu.co>
2020-09-18 17:12:01 -0500

    conexion bd postgresql

---
+++ src/main/java/com/prueba/model/Cliente.java
@@ -0,0 +1,79 @@
+package com.prueba.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+	@Id
+	private int idCliente;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "apellido")
+	private String apellido;
+	
+	@Column(name = "dni")
+	private String dni;
+	
+	@Column(name = "telefono")
+	private String telefono;
+	
+	@Column(name = "email")
+	private String email;
+	
+	
+	
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String relefono) {
+		this.telefono = relefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+	
+	
+}
